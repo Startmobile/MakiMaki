6adf37cc386a33cb179283768f48e378d4d003d8
Aigerim S <98973748+aiger12@users.noreply.github.com>
2023-09-19 14:44:49 +0600

    added color themes

---
+++ app/src/main/java/kz/startmobile/maki/uicomponents/theme/Color.kt
@@ -0,0 +1,31 @@
+package kz.startmobile.maki.uicomponents.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+
+interface ThemeColors {
+    val primary: Color
+    val primaryVariant: Color
+    val secondary: Color
+
+    fun values(): Colors
+
+    object Light : ThemeColors {
+        override val primary = Color(0xFFBB86FC)
+        override val primaryVariant = Color(0xFF3700B3)
+        override val secondary = Color(0xFF03DAC5)
+
+        override fun values() = lightColors(primary, primaryVariant, secondary)
+    }
+
+    object Dark : ThemeColors {
+        override val primary = Color(0xFF6200EE)
+        override val primaryVariant = Color(0xFF3700B3)
+        override val secondary = Color(0xFF03DAC5)
+
+        override fun values(): Colors = darkColors(primary, primaryVariant, secondary)
+    }
+}
